5eca5ea6f93f14afe4782f850cfe7819e7f278ed
Fprasetiawan <fpraseti@terpmail.umd.edu>
2021-09-20 14:13:47 -0400

    png and jpeg endings

--- getAllImages.py
+++ getAllImages.py
@@ -4,7 +4,7 @@ import os
 def getAllImages(imageFolder):
     images = []
     for file in os.listdir(imageFolder):
-        if file.endswith(".jpg"): # might need to change depending on format of images
+        if file.endswith(".jpg") or file.endswith(".png") or file.endswith('.jpeg'): # might need to change depending on format of images
             images.append(os.path.join(imageFolder,file))
 
     return images
