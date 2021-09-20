c3f8a78ead1c0cae9f6650e432af3cc8cd73a5c3
Fprasetiawan <fpraseti@terpmail.umd.edu>
2021-09-20 16:37:37 -0400

    all file types not just pictures

--- getAllImages.py
+++ getAllImages.py
@@ -6,11 +6,7 @@ def getAllImages(imageFolder):
 
     for path, subdirs, files in os.walk(imageFolder):
         for name in files:
-
-            if name.endswith(".jpg") or name.endswith(".png") or name.endswith('.jpeg'): # might need to change depending on format of images
-                images.append(os.path.join(path,name))
-            #print(os.path.join(path, name))
-            #print(os.path.relpath(path, imageFolderPath))
+            images.append(os.path.join(path,name))
 
     
     
